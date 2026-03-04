43ada4d905b5586d0c496f1c72def5716d211185
LonithaPerera <10952829@students.plymouth.ac.uk>
2026-03-04 20:26:47 +0530

    feat: add role column to users table (vehicle_owner, garage, admin)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,10 +36,11 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+    'role' => 'vehicle_owner',
+]);
 
         event(new Registered($user));
 
